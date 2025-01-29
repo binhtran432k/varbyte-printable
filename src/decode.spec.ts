11423dd0d9de3f9368b2c680d43e89ed42987628
Binh Tran <binhtran432k@gmail.com>
2025-01-29 11:43:20 +0700

    test(decode): move expect to outside of func

--- src/decode.spec.ts
+++ src/decode.spec.ts
@@ -4,25 +4,25 @@ import { decodeVarbytePrintable } from "./decode.js";
 
 describe("decodeVarbytePrintable()", () => {
 	test("can decode", () => {
-		testDecode("OPQR", [0, 1, 2, 3]);
+		expect(decodeToNumbers("OPQR", 4)).toEqual([0, 1, 2, 3]);
 	});
 	test("can decode multi-chars into single number", () => {
-		testDecode("}", [45]);
-		testDecode("!O", [46]);
-		testDecode("!}", [91]);
-		testDecode("#O", [92]);
-		testDecode("N}", [2115]);
-		testDecode("! O", [2116]);
+		expect(decodeToNumbers("}", 1)).toEqual([45]);
+		expect(decodeToNumbers("!O", 1)).toEqual([46]);
+		expect(decodeToNumbers("!}", 1)).toEqual([91]);
+		expect(decodeToNumbers("#O", 1)).toEqual([92]);
+		expect(decodeToNumbers("N}", 1)).toEqual([2115]);
+		expect(decodeToNumbers("! O", 1)).toEqual([2116]);
 	});
 	test("can decode big value code", () => {
-		testDecode("?Mn", [0xffff - 1]);
-		testDecode("~", [0xffff]);
-		testDecode("?Mp", [0xffff + 1]);
+		expect(decodeToNumbers("?Mn", 1)).toEqual([0xffff - 1]);
+		expect(decodeToNumbers("~", 1)).toEqual([0xffff]);
+		expect(decodeToNumbers("?Mp", 1)).toEqual([0xffff + 1]);
 	});
 });
 
-function testDecode(input: string, expectedArray: number[]) {
-	const array = new Array<number>(expectedArray.length);
+function decodeToNumbers(input: string, size: number) {
+	const array = new Array<number>(size);
 	decodeVarbytePrintable(input, array);
-	expect(array).toEqual(expectedArray);
+	return array;
 }
